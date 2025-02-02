29b0ad4ea7a0649edaeb8470ce17b02324cd4607
samwhelp <samwhelp@users.noreply.github.com>
2025-02-02 18:05:05 +0000

    fix

--- _demo/scripts/pantheon-adjustment/pantheon-adjustment.sh
+++ _demo/scripts/pantheon-adjustment/pantheon-adjustment.sh
@@ -396,14 +396,37 @@ mod_tool_io_elementary_terminal_config_for_preference () {
 
 	gsettings set io.elementary.terminal.settings follow-last-tab true
 
-	gsettings set io.elementary.terminal.settings background 'rgba(0, 0, 0, 0.85)'
-
-	gsettings set io.elementary.terminal.settings font 'Monospace 12'
-
 	gsettings set io.elementary.terminal.settings remember-tabs false
 
 	gsettings set io.elementary.terminal.settings save-exited-tabs false
 
+
+
+	gsettings set io.elementary.terminal.settings font 'Monospace 12'
+
+
+
+
+	#gsettings set io.elementary.terminal.settings theme "'high-contrast'"
+	gsettings set io.elementary.terminal.settings theme "'dark'"
+	#gsettings set io.elementary.terminal.settings theme "'solarized-light'"
+
+
+	gsettings set io.elementary.terminal.settings theme "'custom'"
+
+
+	#gsettings set io.elementary.terminal.settings background "'#002B36'"
+	gsettings set io.elementary.terminal.settings background "'rgba(0, 0, 0, 0.85)'"
+
+	gsettings set io.elementary.terminal.settings foreground "'#93A1A1'"
+
+	gsettings set io.elementary.terminal.settings cursor-color "'#839496'"
+
+	gsettings set io.elementary.terminal.settings palette "'#073642:#dc322f:#859900:#b58900:#268bd2:#d33682:#2aa198:#eee8d5:#002b36:#dc322f:#586e75:#657b83:#839496:#6c71c4:#93a1a1:#fdf6e3'"
+
+
+
+	gsettings io.elementary.terminal.settings prefer-dark-style true
 
 
 	return 0
@@ -507,11 +530,12 @@ mod_pantheon_config () {
 
 mod_tool_config () {
 
-	mod_tool_io_elementary_terminal_config
 
 	mod_tool_io_elementary_files_config
 
 	#mod_tool_io_elementary_code_config
+
+	#mod_tool_io_elementary_terminal_config
 
 
 	return 0
